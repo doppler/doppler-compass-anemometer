360ccfb4b323023616ef804ee322c9a9f963a761
David Rose <doppler@gmail.com>
2020-07-28 07:24:39 -0400

    style more inline with Grafana's

--- src/CompassAnemometer.tsx
+++ src/CompassAnemometer.tsx
@@ -35,24 +35,69 @@ export const CompassAnemometer: React.FC<Props> = ({
     >
       <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
         <defs>
-          <path
-            id='hash'
-            d='M 250 0 L 256 32 L 262 0 Z'
-            className={styles.direction}
-          />
+          <path id='hash' d='M 250 3 L 256 29 L 262 3 Z' fill='black' />
+          <mask id='hash-mask'>
+            <circle
+              cx='256'
+              cy='256'
+              r='240'
+              stroke='white'
+              stroke-width='32px'
+            />
+
+            <g>
+              <text x='243' y='30' fill='black' className={styles.text}>
+                N
+              </text>
+              <text x='483' y='269' fill='black' className={styles.text}>
+                E
+              </text>
+              <text x='243' y='509' fill='black' className={styles.text}>
+                S
+              </text>
+              <text x='4' y='269' fill='black' className={styles.text}>
+                W
+              </text>
+
+              <use href='#hash' transform='rotate(22.5, 256, 256)' />
+              <use href='#hash' transform='rotate(45, 256, 256)' />
+              <use href='#hash' transform='rotate(67.5, 256, 256)' />
+              <use href='#hash' transform='rotate(112.5, 256, 256)' />
+              <use href='#hash' transform='rotate(135, 256, 256)' />
+              <use href='#hash' transform='rotate(157.5, 256, 256)' />
+              <use href='#hash' transform='rotate(202.5, 256, 256)' />
+              <use href='#hash' transform='rotate(225, 256, 256)' />
+              <use href='#hash' transform='rotate(247.5, 256, 256)' />
+              <use href='#hash' transform='rotate(292.5, 256, 256)' />
+              <use href='#hash' transform='rotate(315, 256, 256)' />
+              <use href='#hash' transform='rotate(337.5, 256, 256)' />
+            </g>
+          </mask>
         </defs>
 
         <path
           className={styles.directionIndicator}
           id='directionIndicator'
-          d='M 258 32 L 260 256 L 314 314 L 256 480 L 197 314 L 252 256 L 254 32 Z'
+          d='M 258 35 L 260 256 L 314 314 L 256 480 L 197 314 L 252 256 L 254 35 Z'
           style={{ transform: `rotate(${dir}deg)` }}
         />
 
-        <circle className={styles.face} id='face' cx='256' cy='256' r='240' />
+        <circle
+          className={styles.face}
+          id='face'
+          cx='256'
+          cy='256'
+          r='240'
+          mask='url(#hash-mask)'
+        />
 
         <circle
-          className={styles.centerCircle}
+          className={cx(
+            styles.centerCircle,
+            css`
+              opacity: 0.75;
+            `
+          )}
           id='centerCircle'
           cx='256'
           cy='256'
@@ -63,39 +108,9 @@ export const CompassAnemometer: React.FC<Props> = ({
           {Math.round(mps * 2.237)}
         </text>
         <text className={styles.velocityLegend} x='256' y='325'>
-          mph
+          {dir}°
         </text>
-
-        <text x='243' y='30' className={cx(styles.direction, styles.text)}>
-          N
-        </text>
-        <text x='483' y='269' className={cx(styles.direction, styles.text)}>
-          E
-        </text>
-        <text x='243' y='509' className={cx(styles.direction, styles.text)}>
-          S
-        </text>
-        <text x='4' y='269' className={cx(styles.direction, styles.text)}>
-          W
-        </text>
-
-        <use href='#hash' transform='rotate(22.5, 256, 256)' />
-        <use href='#hash' transform='rotate(45, 256, 256)' />
-        <use href='#hash' transform='rotate(67.5, 256, 256)' />
-        <use href='#hash' transform='rotate(112.5, 256, 256)' />
-        <use href='#hash' transform='rotate(135, 256, 256)' />
-        <use href='#hash' transform='rotate(157.5, 256, 256)' />
-        <use href='#hash' transform='rotate(202.5, 256, 256)' />
-        <use href='#hash' transform='rotate(225, 256, 256)' />
-        <use href='#hash' transform='rotate(247.5, 256, 256)' />
-        <use href='#hash' transform='rotate(292.5, 256, 256)' />
-        <use href='#hash' transform='rotate(315, 256, 256)' />
-        <use href='#hash' transform='rotate(337.5, 256, 256)' />
       </svg>
-
-      <div className={styles.textBox}>
-        <div>{dir}°</div>
-      </div>
     </div>
   );
 };
@@ -110,18 +125,11 @@ const getStyles = stylesFactory((theme) => {
       top: 0;
       left: 0;
     `,
-    textBox: css`
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      padding: 10px;
-    `,
     direction: css`
-      fill: hsla(240, 100%, 90%, 0.5);
+      fill: black;
     `,
     directionIndicator: css`
-      stroke: white;
-      fill: hsla(30, 100%, 50%, 0.85);
+      fill: ${theme.isLight ? theme.palette.greenBase : theme.palette.blue95};
       transform-origin: 256px 256px;
       transform: rotate(0deg);
     `,
@@ -131,14 +139,12 @@ const getStyles = stylesFactory((theme) => {
       stroke-width: 32px;
     `,
     centerCircle: css`
-      fill: hsla(255, 100%, 50%, 0.75);
+      fill: ${theme.isLight ? theme.palette.greenBase : theme.palette.blue95};
     `,
     velocityText: css`
       font-size: 9rem;
-      /* stroke: white; */
-      fill: hsl(120, 100%, 50%);
+      fill: black;
       font-weight: bold;
-      /* font-family: 'Courier New', Courier, monospace; */
       text-anchor: middle;
       dominant-baseline: middle;
     `,
@@ -148,9 +154,8 @@ const getStyles = stylesFactory((theme) => {
       font-size: 3rem;
     `,
     velocityLegend: css`
-      fill: white;
+      fill: black;
       font-size: 1.5em;
-      font-family: 'Courier New', Courier, monospace;
       font-weight: bold;
       text-anchor: middle;
     `,
